b8ae3456d4ba640333517115b1c2726371a8a62c
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-10-02 22:58:52 -0300

    feat: repository cache

--- src/domain/notification/application/repositories/notifications-repository.ts
+++ src/domain/notification/application/repositories/notifications-repository.ts
@@ -1,4 +1,4 @@
-import { Notification } from '../../enterprise/entities/notification';
+import { Notification } from '../../enterprise/entities/notification'
 
 export abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>
