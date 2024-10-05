c3b790f3f16c6573acb39508bc7827f4d166ed2b
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-10-04 22:09:03 -0300

    feat: teste cache repository

---
+++ src/infra/database/prisma/repositories/prisma-questions-repository.e2e-spec.ts
@@ -0,0 +1,70 @@
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { AppModule } from '@/infra/app.module'
+import { CacheRepository } from '@/infra/cache/cache-repository'
+import { CacheModule } from '@/infra/cache/cache.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { AttachmentFactory } from 'test/factories/make-attachment'
+import { QuestionFactory } from 'test/factories/make-question'
+import { QuestionAttachmentFactory } from 'test/factories/make-question-attachment'
+import { StudentFactory } from 'test/factories/make-student'
+
+describe('Prisma Questions Repository (E2E)', () => {
+  let app: INestApplication
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let attachmentFactory: AttachmentFactory
+  let questionAttachmentFactory: QuestionAttachmentFactory
+  let cacheRepository: CacheRepository
+  let questionsRepository: QuestionsRepository
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule, CacheModule],
+      providers: [
+        StudentFactory,
+        QuestionFactory,
+        AttachmentFactory,
+        QuestionAttachmentFactory,
+      ],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    studentFactory = moduleRef.get<StudentFactory>(StudentFactory)
+    questionFactory = moduleRef.get<QuestionFactory>(QuestionFactory)
+    attachmentFactory = moduleRef.get<AttachmentFactory>(AttachmentFactory)
+    questionAttachmentFactory = moduleRef.get<QuestionAttachmentFactory>(
+      QuestionAttachmentFactory,
+    )
+    cacheRepository = moduleRef.get<CacheRepository>(CacheRepository)
+    questionsRepository =
+      moduleRef.get<QuestionsRepository>(QuestionsRepository)
+
+    await app.init()
+  })
+
+  it('should get a question by slug', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const question = await questionFactory.makePrismaQuestion({
+      slug: Slug.create('question-1'),
+      authorId: user.id,
+    })
+
+    const attachment = await attachmentFactory.makePrismaAttachment()
+
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      attachmentId: attachment.id,
+      questionId: question.id,
+    })
+
+    const slug = question.slug.value
+
+    const questionDetails = await questionsRepository.findDetailsBySlug(slug)
+    const cached = await cacheRepository.get(`question:${slug}:details`)
+
+    expect(cached).toEqual(JSON.stringify(questionDetails))
+  })
+})
